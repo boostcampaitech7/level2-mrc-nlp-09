b7512f3b026e3822ea12c30dbb5cdffe19ea2f95
jeongeun <irin7348@naver.com>
2024-10-15 10:59:25 +0900

    development branch

--- utils/io/file_utils.py
+++
@@ -1,9 +0,0 @@
-# 텍스트 파일 읽기
-def read_file(file_path):
-    with open(file_path, 'r') as f:
-        return f.read()
-
-# 텍스트 파일 쓰기
-def write_file(data, file_path):
-    with open(file_path, 'w') as f:
-        f.write(data)
