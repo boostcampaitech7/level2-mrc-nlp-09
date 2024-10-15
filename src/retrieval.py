53bf8984c7b5e1456b9f526a6abcb5b7612d6fa6
jeongeun <irin7348@naver.com>
2024-10-15 14:49:34 +0900

    develop branch

--- src/retrieval.py
+++ src/retrieval.py
@@ -30,7 +30,7 @@ class SparseRetrieval:
     def __init__(
         self,
         tokenize_fn,
-        data_path: Optional[str] = "../data/",
+        data_path: Optional[str] = "data/raw/",
         context_path: Optional[str] = "wikipedia_documents.json",
     ) -> NoReturn:
 
