94afd1d3da7572015214d1960ed047622fc30441
gwaksital <y10cuk@naver.com>
2024-10-16 15:04:13 +0000

    Add feature in retrieval.py: Evaluate wiki.json score, Analyze score result with .csv, Modify arg "default", Make bash file, Add preprocess file: preprocess_title-text.py.  note: args.use_faiss is always False in the code.

---
+++ exp_title-text.sh
@@ -0,0 +1,7 @@
+python src/retrieval.py --data_path data/preprocessed --context_path wikipedia_documents_no_duplication.json --topk 25
+python src/retrieval.py --data_path data/preprocessed --context_path wikipedia_documents_no_duplication.json --topk 30
+python src/retrieval.py --data_path data/preprocessed --context_path wikipedia_documents_no_duplication.json --topk 35
+
+python src/retrieval.py --topk 25
+python src/retrieval.py --topk 30
+python src/retrieval.py --topk 35
