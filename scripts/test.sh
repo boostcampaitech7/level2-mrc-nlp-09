134c66c3fc20b5d90498f5a8ea7e04d5eab9e45e
gwaksital <y10cuk@naver.com>
2024-10-24 06:44:44 +0000

    Dense Retrieval Experiment Done -> Output for test dataset: Top1,2 = hard_voting, Top3 = original, Top5~ = BGEm3ko

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,8 +2,6 @@
 
 set +e
 
-python src/retrieval_test_KoE5_pipeline.py || true
-
 python src/retrieval_test_ko-reranker_pipeline.py || true
 
 python src/retrieval_test_bge-reranker_pipeline.py || true
