9598676347955cb8979017d9d5dd8108b0ca4288
wjddms4299 <121777522+wjddms4299@users.noreply.github.com>
2024-10-28 11:14:09 +0900

    final_5

--- scripts/run_retriever.sh
+++ scripts/run_retriever.sh
@@ -1,4 +1,4 @@
 python src/run_retrieval.py --strategies ST_02 --debug False --report False --run_cnt 1
 python src/run_retrieval.py --strategies ST_03 --debug False --report False --run_cnt 1
 python src/run_retrieval.py --strategies ST_07 --debug False --report False --run_cnt 1
-python src/run_retrieval.py --strategies ST_09 --debug False --report False --run_cnt 1
+python src/run_retrieval_csv.py --strategies ST_09 --debug False --report False --run_cnt 1
